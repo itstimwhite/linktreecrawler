c8e6e2ec8f4971b19c1dce771fc90d2455fa8463
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-12-15 15:19:26 -0800

    a

--- lintreeCrawler/src/main.js
+++ lintreeCrawler/src/main.js
@@ -1,15 +1,23 @@
 // For more information, see https://crawlee.dev/
 import { CheerioCrawler, ProxyConfiguration } from "crawlee";
 import { router } from "./routes.js";
+import { KeyValueStore } from "crawlee";
 
+const input = await KeyValueStore.getInput();
+console.log(input);
 const MAX_PAGES = 4860;
-const startUrls = [];
-
-//https://linktr.ee/discover/profile-directory/c/all/page-[PAGE_NUMBER]/
-//loop through the pages and add them to the startUrls array
-for (let i = 1; i <= MAX_PAGES; i++) {
-  startUrls.push(`https://linktr.ee/discover/profile-directory/c/all/page-${i}/`);
+//if input is not empty, use it as the startUrls array else use the default
+//for (let i = 1; i <= MAX_PAGES; i++) {
+//  startUrls.push(`https://linktr.ee/discover/profile-directory/c/all/page-${i}/`);
+//}
+if (input.length > 0) {
+  var startUrls = input;
+} else {
+  for (let i = 1; i <= MAX_PAGES; i++) {
+    startUrls.push(`https://linktr.ee/discover/profile-directory/c/all/page-${i}/`);
+  }
 }
+
 console.log(startUrls.length);
 
 const crawler = new CheerioCrawler({
