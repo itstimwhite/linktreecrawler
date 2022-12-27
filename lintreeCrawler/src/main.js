7d53097651a7e72e4f558aa2e5f3451bfa195a5c
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-12-26 20:43:00 -0800

    push

--- lintreeCrawler/src/main.js
+++ lintreeCrawler/src/main.js
@@ -2,27 +2,22 @@
 import { CheerioCrawler, ProxyConfiguration } from "crawlee";
 import { router } from "./routes.js";
 import { KeyValueStore } from "crawlee";
-
+const startUrls = [];
 const input = await KeyValueStore.getInput();
 console.log(input);
-const MAX_PAGES = 4860;
-//if input is not empty, use it as the startUrls array else use the default
-//for (let i = 1; i <= MAX_PAGES; i++) {
-//  startUrls.push(`https://linktr.ee/discover/profile-directory/c/all/page-${i}/`);
-//}
-if (input.length > 0) {
-  var startUrls = input;
-} else {
-  for (let i = 1; i <= MAX_PAGES; i++) {
-    startUrls.push(`https://linktr.ee/discover/profile-directory/c/all/page-${i}/`);
-  }
-}
+const MAX_PAGES = 6;
 
-console.log(startUrls.length);
+for (let i = 1; i <= MAX_PAGES; i++) {
+  startUrls.push(`https://linktr.ee/discover/profile-directory/c/all/page-${i}/`);
+}
 
 const crawler = new CheerioCrawler({
   // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
   requestHandler: router,
+  useSessionPool: true,
 });
 
 await crawler.run(startUrls);
+//combine all the data into one array and then save it to a json file
+const data = await Dataset.getData();
+await Apify.setValue("OUTPUT", data);
--- lintreeCrawler/src/routes.js
+++ lintreeCrawler/src/routes.js
@@ -1,4 +1,4 @@
-import { Dataset, createCheerioRouter } from "crawlee";
+import { Dataset, createCheerioRouter, utils } from "crawlee";
 
 export const router = createCheerioRouter();
 
@@ -10,15 +10,43 @@ router.addDefaultHandler(async ({ enqueueLinks, log }) => {
   });
 });
 
-//write the above function but this time log an index number of each link
-
 router.addHandler("detail", async ({ request, $, log }) => {
+  log.info(`processing detail page ${request.loadedUrl}`);
   const title = $("title").text();
-  //get the title of the page
-  log.info(`pushing data for ${request.loadedUrl}`);
+  //get the html of the page
+  const html = $.html();
 
+  //get the profile photo URL
+  const profilePhotoUrl = $("[data-testid='ProfileImage']").attr("src");
+
+  //get the name from the h1 tag
+  const name = $("h1").text();
+
+  //check if there is a logo in the footer with data-testid="Logo--white" or data-testid="Logo--black"
+  const logo = $("[data-testid='Logo--white'], [data-testid='Logo--black']");
+  let accountType;
+  if (logo.length > 0) {
+    //if the logo exists, set the account type to "free"
+    accountType = "free";
+  } else {
+    //if the logo does not exist, set the account type to "paid"
+    accountType = "paid";
+  }
+
+  const socialLinks = utils.social.parseHandlesFromHtml($("div.dLqIok").html());
+
+  //use the parseHandlesFromHtml function to extract the social media handles from the array
+  /* const social = utils.social.parseHandlesFromHtml(socialIcons.html()); */
+
+  log.info(`pushing data for ${request.loadedUrl}`);
+  //push data to the dataset with all the extracted information
   await Dataset.pushData({
     url: request.loadedUrl,
     title,
+    socialLinks,
+
+    profilePhotoUrl,
+    name,
+    accountType,
   });
 });
